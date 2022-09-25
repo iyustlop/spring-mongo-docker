1a6c686853db76da14bf0424b7b3bd186e642f34
Ignacio Yuste Lopez <iyustlop@gmail.com>
2022-09-26 00:15:29 +0200

    First session telusko youtube video. I have added Service class

--- src/test/java/com/olivar/springbootmongodbdocker/SpringbootMongodbDockerApplicationTests.java
+++ src/test/java/com/olivar/springbootmongodbdocker/SpringbootMongodbDockerApplicationTests.java
@@ -1,13 +1,37 @@
 package com.olivar.springbootmongodbdocker;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.olivar.springbootmongodbdocker.dao.ManufacturerRepo;
+import com.olivar.springbootmongodbdocker.model.Manufacturer;
+import com.olivar.springbootmongodbdocker.service.ManufacturerService;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringbootMongodbDockerApplicationTests {
 
+	@Autowired
+	private ManufacturerService service;
+
+	@MockBean
+	private ManufacturerRepo repository;
+
 	@Test
-	void contextLoads() {
+	public void getManufacturerTest() {
+		when(repository.findAll()).thenReturn(Stream.of(new Manufacturer("Ferrari","F01")).collect(Collectors.toList()));
+
+		assertEquals(1, service.getAllManufacturer().size());
 	}
 
 }

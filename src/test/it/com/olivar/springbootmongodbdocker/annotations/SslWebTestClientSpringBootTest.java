7d120343b0282d6e6ef2d229e12c6e1f72788517
Ignacio Yuste Lopez <iyustlop@gmail.com>
2024-06-05 23:44:28 +0200

    Update framework and changed application.yml in test

--- src/test/it/com/olivar/springbootmongodbdocker/annotations/SslWebTestClientSpringBootTest.java
+++ src/test/it/com/olivar/springbootmongodbdocker/annotations/SslWebTestClientSpringBootTest.java
@@ -3,6 +3,7 @@ package com.olivar.springbootmongodbdocker.annotations;
 import com.olivar.springbootmongodbdocker.config.WebTestClientConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -13,5 +14,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Import(WebTestClientConfiguration.class)
+@TestPropertySource("/application-test.yml")
 public @interface SslWebTestClientSpringBootTest {
 }

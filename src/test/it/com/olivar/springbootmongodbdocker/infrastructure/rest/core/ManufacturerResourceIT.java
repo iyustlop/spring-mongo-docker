61e0be8a2ba98cf1cf6bef7346f2e31483599c85
Ignacio Yuste Lopez <iyustlop@gmail.com>
2024-11-12 11:52:16 +0100

    Creation of the two integrations test.

--- src/test/it/com/olivar/springbootmongodbdocker/infrastructure/rest/core/ManufacturerResourceIT.java
+++ src/test/it/com/olivar/springbootmongodbdocker/infrastructure/rest/core/ManufacturerResourceIT.java
@@ -1,49 +1,80 @@
 package com.olivar.springbootmongodbdocker.infrastructure.rest.core;
 
-import com.olivar.springbootmongodbdocker.annotations.SslWebTestClientSpringBootTest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.olivar.springbootmongodbdocker.infrastructure.rest.core.dto.ManufactureObjectMother;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SslWebTestClientSpringBootTest
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@Testcontainers
 class ManufacturerResourceIT {
 
-	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo");
+    @Autowired
+    private MockMvc mockMvc;
 
-	@DynamicPropertySource
-	static void  configureProperties(DynamicPropertyRegistry registry){
-		registry.add("spring.data.mongodb.uri", mongoDBContainer::getConnectionString);
-	}
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getConnectionString);
+    }
 
-	@BeforeAll
-	static void beforeAll(){
-		mongoDBContainer.start();
-	}
+    @BeforeAll
+    static void beforeAll() {
+        mongoDBContainer.start();
+    }
 
-	@AfterAll
-	static void afterAll(){
-		mongoDBContainer.stop();
-	}
+/*    @AfterAll
+    static void afterAll() {
+        System.out.println("apago el docker");
+        mongoDBContainer.stop();
+    }*/
 
-	@Autowired
-	private WebTestClient webTestClient;
 
-	@Test
-	@DisplayName("Given a valid manufacturer When creates Then response with HTTP200 and the agent detail")
-	void create_a_valid_agent() {
-		webTestClient.post().uri("/manufacturer")
-				.contentType(MediaType.APPLICATION_JSON)
-				.accept(MediaType.APPLICATION_JSON)
-				.bodyValue(ManufactureObjectMother.randomManufacturerRequest())
-				.exchange()
-				.expectStatus().isOk();
-	}
+    @Test
+    @DisplayName("Given a valid manufacturer When creates Then response with HTTP200 and the agent detail")
+    void create_a_valid_agent() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/manufacturer")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(asJsonString(ManufactureObjectMother.randomManufacturerRequest()))
+                    .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Get the manufacturer stored in the database")
+    void get_previous_manufacturer() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                    .get("/manufacturer"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(1));
+    }
+
+    private String asJsonString(Object object) {
+        try {
+            return new ObjectMapper().writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
